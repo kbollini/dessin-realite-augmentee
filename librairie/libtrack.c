5d025e4da8750139a40b7aacafc3b381c844b919
kevinbollini <kevinbollini@gmail.com>
2012-02-21 21:45:47 +0000

    premier test (compile pas.)

--- librairie/libtrack.c
+++ librairie/libtrack.c
@@ -1,21 +1,37 @@
 #include "libtrack.h"
 
-IplImage * Binarisation (IplImage * source, int h, int s, int v)
+IplImage * Binarisation(IplImage * source, int x, int y)
+{
+	IplImage *hsv;
+	hsv = cvCloneImage(source);
+	cvCvtColor(source, hsv, CV_BGR2HSV);
+	CvScalar pixel;
+	pixel = cvGet2D(hsv,x,y); // on recupère le pixel
+	
+	int h = (int)pixel.val[0];
+	int s = (int)pixel.val[1];
+	int v = (int)pixel.val[2];
+	int tolerance = 10;
+	IplImage *mask = NULL;
+	mask = cvCreateImage(cvGetSize(source), source->depth, 1);
+	cvInRangeS(hsv, cvScalar(h - tolerance -1, s - tolerance, 0), cvScalar(h + tolerance -1, s + tolerance, 255), mask);
+	return mask;
+}
+
+IplImage * Localisation(IplImage * binarySource, int x, int y)
 {
 
 }
 
-IplImage * Localisation (IplImage * binarySource, int x, int y)
+int * NaifColorTrack(IplImage * source, int x, int y)
 {
 
 }
 
-int * NaifColorTrack (IplImage * source, int x, int y)
+int main(int argc, char* argv[])
 {
-
-}
-
-int main()
-{
-
+	IplImage* source = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+	cvNamedWindows("test", CV_WINDOW_AUTOSIZE);
+	cvShowImage("test", Binarisation(source, 25, 25));
+	
 }
--- librairie/libtrack.h
+++ librairie/libtrack.h
@@ -3,7 +3,7 @@
 #include <opencv/highgui.h>
 #include <opencv/cv.h>
 
-IplImage * Binarisation (IplImage * source, int h, int s, int v);
-IplImage * Localisation (IplImage * binarySource, int x, int y);
+IplImage * Binarisation(IplImage * source, int x, int y);
+IplImage * Localisation(IplImage * binarySource, int x, int y);
 
-int * NaifColorTrack (IplImage * source, int x, int y); 
+int * NaifColorTrack(IplImage * source, int x, int y); 
