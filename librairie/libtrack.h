87acb0a064816ec24eac48b3dbb2ed632ed7da91
geoffrey.melia <geoffrey.melia@gmail.com>
2012-02-22 19:35:15 +0000

    supprssion des .c et .h et ajout des .cpp et .hpp

--- librairie/libtrack.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <opencv/highgui.h>
-#include <opencv/cv.h>
-
-IplImage * Binarisation(IplImage * source, int x, int y);
-IplImage * Localisation(IplImage * binarySource, int x, int y);
-int * NaifColorTrack(IplImage * source, int x, int y); 
