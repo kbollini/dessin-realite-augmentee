5932ff0d7d47b010c708c5ec75bfde523440338a
baptiste.saleil <baptiste.saleil@9c75aff5-4abd-7361-e767-a6f305702f6d>
2012-02-23 23:35:25 +0000

    Ajout choix webcam et fonction demarrage.

--- logiciel/client.cpp
+++ logiciel/client.cpp
@@ -1,10 +1,55 @@
 #include "client.hpp"
 
-Client::Client()
+Client::Client() : ui(new Ui::Client)
 {
-	ui.setupUi(this);
+	ui->setupUi(this);
 	
-	wm = new WebcamManager();
+	camManager = new WebcamManager();
+	camWidget = new WidgetWebcam();
 	
-	std::cout << wm->getNumberOfWebcams() << std::endl;
+	// Pour lister les webcams
+	actionGroup = new QActionGroup(ui->menuWebcam);
+	actionGroup->setExclusive(true);
+	
+	int nbCams = camManager->getNumberOfWebcams();
+	int i=0;
+	for (i=0; i<nbCams; i++)
+	{
+		// TODO : lister le nom des webcams plutôt que le numéro (opencv)
+		QAction *action = new QAction("webcam" + QString::number(i+1), ui->menuWebcam);
+		action->setCheckable(true);
+		actionGroup->addAction(action);
+		
+		// Si c'est la première webcam, on l'active par défaut
+		if (i == 0) {action->setChecked(true);}
+		
+		ui->menuWebcam->addAction(action);
+	}
+	
+	connect(ui->actionD_marrer,SIGNAL(triggered()),this,SLOT(slotStart()));
+}
+
+void Client::slotStart()
+{
+	std::cout << "debug" << std::endl;
+	// On désactive le choix des webcams
+	ui->menuWebcam->setEnabled(false);
+	
+	// On récupère une l'image de la webcam
+	QList<QAction*> listActionsWebcam = actionGroup->actions();
+	int webcam = 0;
+	int i=0;
+	for (i=0; i<listActionsWebcam.length(); i++)
+	{
+		if (listActionsWebcam[i]->isChecked())
+		{ webcam = listActionsWebcam[i]->text().remove("webcam").toInt(); }
+	}
+	qDebug() << webcam;
+	//IplImage *image = camManager->getOneImage(webcam-1);
+	
+	// Appel widgetwebcam avec image
+	// int ret = camWidget->calibrate(image);
+	
+	// Traiter retour...
+	
 }
