22c9625145db05723049fb6a48c4c0bf4f447213
julien.projet@gmail.com <julien.projet@gmail.com@9c75aff5-4abd-7361-e767-a6f305702f6d>
2012-02-26 10:13:37 +0000

    Ajouts de petites modifications, et d'une fonction pour dessiner une ligne

--- trunk/logiciel/localdrawingboard.cpp
+++ trunk/logiciel/localdrawingboard.cpp
@@ -13,11 +13,14 @@ void LocalDrawingBoard::drawPoint(int x, int y)
 	scene->addEllipse(x, y, 5, 5, QPen(), QBrush(Qt::SolidPattern));
 }
 
+// Dessine un point sur le tableau
 void LocalDrawingBoard::drawQPoint(QPoint p)
 {
 	scene->addEllipse(p.x(), p.y(), 5, 5, QPen(), QBrush(Qt::SolidPattern));
 }
 
+// Dessine une ligne à partir de deux points (deux mouvements successifs)
 void LocalDrawingBoard::drawLine(int fromX, int fromY, int toX, int toY)
 {
+	scene->addLine(fromX, fromY, toX, toY, QPen());
 }
