e1b95b6914fa32f93a5255e7af96dd924cd798a3
baptiste.saleil <baptiste.saleil@9c75aff5-4abd-7361-e767-a6f305702f6d>
2012-03-24 21:07:04 +0000

    Nettoyage

--- logiciel/main.cpp
+++ logiciel/main.cpp
@@ -1,15 +1,13 @@
 #include <QApplication>
 #include "client.hpp"
-#include "webcams.hpp"
+#include "calibration.hpp"
 
 int main(int argc, char**argv)
 {
 	QApplication app(argc,argv);
-	//Client c;
-	//c.showMaximized();
 	
-	Webcams w;
-	w.show();
+	Calibration c;
+	c.show();
 	
 	return app.exec();
 }
