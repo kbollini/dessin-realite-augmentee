b114e1379202de4c5df9bf012bdd52a93e080675
julien.projet <julien.projet@gmail.com>
2012-04-11 10:57:45 +0000

    Amélioration du code du serveur, ajouts sur le package manager du serveur

--- logiciel/server/packagemanager.cpp
+++ logiciel/server/packagemanager.cpp
@@ -15,3 +15,34 @@ void PackageManager::sendPoint(QDataStream &stream, QPoint point)
 	stream << command << type;
 	stream << point;
 }
+
+void PackageManager::order(QDataStream &stream, QList<QTcpSocket *> &clients, ServerGraphics* graphics)
+{
+	// Type de l'ordre
+	QString type;
+	stream >> type;
+	
+	if(type == "qpoint")
+	{
+		QPoint p;	
+		stream >> p;
+			
+		// Ajout du point sur la scène
+		graphics->addPoint(p);
+			
+		// Envoi à tous les clients du point à dessiner
+		broadcastPoint(clients, p);
+	}
+}
+
+void PackageManager::broadcastPoint(QList<QTcpSocket *> &clients, QPoint point)
+{
+	for(int i=0; i <clients.size(); ++i)
+	{
+		QDataStream stream(clients.at(i));
+		
+		// Envoi du point à un client
+		sendPoint(stream, point);
+	}
+}
+
--- logiciel/server/packagemanager.hpp
+++ logiciel/server/packagemanager.hpp
@@ -9,19 +9,26 @@ using namespace std;
 */
 
 #include <QDataStream>
+#include <QList>
 #include <QPixmap>
 #include <QString>
+#include <QtNetwork>
+#include "servergraphics.hpp"
 
 class PackageManager
 {
-	public :
+	public :		
+		// Envoi d'un point à tous les clients
+		static void broadcastPoint(QList<QTcpSocket *> &clients, QPoint point);
+
+		// Envoi d'un point à un seul client
+		static void sendPoint(QDataStream &stream, QPoint point);
+
 		// Envoi au client un objet pixmap
 		static void sendPixmap(QDataStream &stream, QPixmap pixmap);
 		
-		// Envoi d'un point
-		static void sendPoint(QDataStream &stream, QPoint point);
-
-
+		// Traite une commande de type ordre, en agissant sur la scène
+		static void order(QDataStream &stream, QList<QTcpSocket *> &clients, ServerGraphics* graphics);
 };
 
 
