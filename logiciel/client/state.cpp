8aaeb11e5cdb45548c53e1304c0cbaacecc36190
baptiste.saleil <baptiste.saleil@9c75aff5-4abd-7361-e767-a6f305702f6d>
2012-04-17 17:47:50 +0000

    Ajout des outils sur l'interface, communication avec la classe état. Manque qu'à intérroger la classe pour faire passer sur le réseau.

--- logiciel/client/state.cpp
+++ logiciel/client/state.cpp
@@ -1,4 +1,34 @@
 #include "state.hpp"
 
 State::State()
-{}
+{
+	// On affecte les paramètres par défaut : Curseur noir de taille 5
+	width = 5;
+	color.setNamedColor("#000000");
+}
+
+QColor State::getColor()
+{
+	return color;
+}
+
+int State::getWidth()
+{
+	return width;
+}
+
+void State::setColor(QColor c)
+{
+	color = c;
+}
+
+void State::setWidth(int w)
+{
+	width = w;
+}
+
+void State::toString()
+{
+	qDebug() << "Diametre : " << QString::number(width);
+	qDebug() << "Couleur : " << color.name();
+}
--- logiciel/client/state.hpp
+++ logiciel/client/state.hpp
@@ -7,16 +7,32 @@
 */
 
 #include <iostream>
+#include <QColor>
+#include <QDebug>
 
 using namespace std;
 
-class State
+class State : public QObject
 {
+	Q_OBJECT	
+	
 	public :
 		State();
+		// Accès aux paramètres
+		QColor getColor();
+		int getWidth();
+		void setColor(QColor);
+		//void setWidth(int);
+		
+		void toString();	// Affichage
+	
+	public slots :
+		void setWidth(int);
 		
 	private :
-		/* Différents paramètres du dessin */
+		// Différents paramètres du dessin
+		int width;
+		QColor color;
 		
 		
 };
