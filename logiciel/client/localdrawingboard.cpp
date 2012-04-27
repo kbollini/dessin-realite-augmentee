2da784cca66cba7945927de14ee43fe28e183c56
baptiste.saleil <baptiste.saleil@9c75aff5-4abd-7361-e767-a6f305702f6d>
2012-04-27 13:49:00 +0000

    Ne dessine plus sur l'interface.

--- logiciel/client/localdrawingboard.cpp
+++ logiciel/client/localdrawingboard.cpp
@@ -7,36 +7,30 @@ LocalDrawingBoard::LocalDrawingBoard()
 
 void LocalDrawingBoard::drawPoint(int x, int y)
 {	
-	if(firstPoint == true)
+	bool canDraw = !(x > gestureWidget->pos().x() && x < gestureWidget->size().width() &&
+			y > gestureWidget->pos().y() && y < gestureWidget->size().height());
+	
+	if (canDraw)
 	{
-		scene->addEllipse(x, y, pen->width(), pen->width(), *pen, QBrush(Qt::SolidPattern));
-		precedent = new QPoint(x, y);
-		firstPoint = false;
-	}
-	else
-	{
-		drawLine(precedent->x(), precedent->y(), x, y);
-		precedent->setX(x);
-		precedent->setY(y);
+		if(firstPoint == true)
+		{
+			scene->addEllipse(x, y, pen->width(), pen->width(), *pen, QBrush(Qt::SolidPattern));
+			precedent = new QPoint(x, y);
+			firstPoint = false;
+		}
+		else
+		{
+			drawLine(precedent->x(), precedent->y(), x, y);
+			precedent->setX(x);
+			precedent->setY(y);
+		}
 	}
 }
 
 // Dessine un point sur le tableau
 void LocalDrawingBoard::drawQPoint(QPoint p)
-{	
-	
-	if(firstPoint == true)
-	{
-		scene->addEllipse(p.x(), p.y(), pen->width(), pen->width(), *pen, QBrush(Qt::SolidPattern));
-		precedent = new QPoint(p);
-		firstPoint = false;
-	}
-	else
-	{
-		drawLine(precedent->x(), precedent->y(), p.x(), p.y());
-		precedent->setX(p.x());
-		precedent->setY(p.y());	
-	}
+{
+	drawPoint(p.x(),p.y());
 }
 
 // Dessine une ligne à partir de deux points (deux mouvements successifs)
